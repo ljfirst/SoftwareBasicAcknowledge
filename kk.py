5834559f51a053c35014b74a3a79b27b6ca95d5e
ljfirst <ljfirst@mail.ustc.edu.cn>
2019-09-27 13:54:59 +0800

    hh

---
+++ kk.py
@@ -0,0 +1,27 @@
+cd Algorithm
+echo "======Algorithm"
+git status
+
+cd ../CompanyExamination
+echo "======CompanyExamination"
+git status
+
+cd ../DesignPattern
+echo "=====DesignPattern"
+git status
+
+cd ../MedicationSystem
+echo "=====MedicationSystem"
+git status
+
+cd ../MasterPaper
+echo "=====MasterPaper"
+git status
+
+cd ../R&U_MachineLearn
+echo "=====R&U_MachinaLearn"
+git status
+
+cd ../
+
+
